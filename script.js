9b28a73f0218f12a770ef4037b2a162ce973f034
deekshyantdahal-debug <deekshyantdahal@gmail.com>
2025-12-04 17:34:21 -0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Virtual Madal Ready");
+let ctx = new (window.AudioContext || window.webkitAudioContext)();
+
+function playTone(freq){
+  let osc = ctx.createOscillator();
+  let gain = ctx.createGain();
+  osc.frequency.value = freq;
+  gain.gain.setValueAtTime(1, ctx.currentTime);
+  gain.gain.exponentialRampToValueAtTime(0.001, ctx.currentTime + 0.25);
+  osc.connect(gain).connect(ctx.destination);
+  osc.start();
+  osc.stop(ctx.currentTime + 0.3);
+}
+
+function playLeft(){ playTone(140); }
+function playRight(){ playTone(200); }
